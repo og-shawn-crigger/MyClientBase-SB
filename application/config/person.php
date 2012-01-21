b63d7f8d52c627233fa0c8472dd575de79e56ba7
Damiano Venturin <dam@venturin.net>
2012-01-21 20:58:09 +0100

    OPEN - task 43: Edit contact: verify that everything works. Enable the link https://github.com/damko/MyClientBase-SB/issues/issue/43

--- application/config/person.php
+++ application/config/person.php
@@ -1,4 +1,4 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['person_show_fields'] = array('title','cn','mail','mobile','homePhone','spouseName','secretary','homePostalAddress','mozillaHomePostalCode','mozillaHomeLocalityName','mozillaHomeState','mozillaHomeCountryName','preferredLanguage','note','assistantName','assistantPhone','codiceFiscale','uid','dbId','calendarURI');
+$config['person_show_fields'] = array('jpegPhoto','title','sn','givenName','mail','labeledURI','calendarURI','mobile','homePhone','homeFacsimileTelephoneNumber','homePostalAddress','mozillaHomePostalCode','mozillaHomeLocalityName','mozillaHomeState','mozillaHomeCountryName','preferredLanguage','birthDate','codiceFiscale','spouseName','assistantName','assistantPhone','companyPhone','category','enabled','note');
 $config['person_hidden_fields'] = array('uid','client_id');
