9488ccd00eda863738e74652d92758f2c03e68ca
Damiano Venturin <dam@venturin.net>
2012-03-12 00:07:52 +0100

    refs #51 forgot some files

---
+++ application/config/CopyOflocation.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['location_show_fields'] = array(
+											'objectClass',
+											'locZip',
+											'locStreet',
+											'locState',
+											'locPhone',
+											'locLongitude',
+											'locLatitude',
+											'locId',
+											'locDescription',
+											'locCountry',
+											'locCity',
+											'entryUpdatedBy',
+											'entryCreatedBy');
+$config['location_hidden_fields'] = array('locId');
