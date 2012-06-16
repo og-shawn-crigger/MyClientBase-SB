41943a042c6c76c2d9ad402ef956167a440c9952
Damiano Venturin <dam@venturin.net>
2012-06-16 18:30:38 +0200

    ref #66 updated application/config to MyClientBase 012

--- application/config/citranslate.php
+++ application/config/citranslate.php
@@ -2,6 +2,7 @@
 
 $config['languageDirName_en'] = FCPATH.APPPATH.'language/english/';
 $config['languageDirName_it'] = FCPATH.APPPATH.'language/italian/';
+$config['languageDirName_ru'] = FCPATH.APPPATH.'language/russian/';
 
 /* End of file citranslate.php */
 /* Location: ./application/config/citranslate.php */
--- application/config/phpgettext.php
+++ application/config/phpgettext.php
@@ -2,10 +2,11 @@
 
 $config['gettextProjectDir'] = FCPATH.APPPATH.'third_party/php-gettext-1.0.11';
 $config['gettextLocaleDir'] = FCPATH.APPPATH.'third_party/php-gettext-1.0.11/locale';
-$config['gettextDefaultLocale'] = 'en_EN';
+//$config['gettextDefaultLocale'] = 'en_US';
+$config['gettextDefaultLocale'] = 'ru_RU';
 //$config['gettextDefaultLocale'] = 'it_IT';
 $config['gettextInc'] = FCPATH.APPPATH.'third_party/php-gettext-1.0.11/gettext.inc';
-$config['gettextSupportedLocales'] = array('en_US', 'it_IT');
+$config['gettextSupportedLocales'] = array('en_US', 'it_IT', 'ru_RU');
 $config['gettextEncoding'] = 'UTF-8';
 
 /* End of file phpgettext.php */
