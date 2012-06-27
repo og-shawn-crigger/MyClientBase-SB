260e60245311b6ba65da0940a8b412b1e54331fb
Damiano Venturin <dam@venturin.net>
2012-06-27 15:25:54 +0200

    Closes #84

--- application/modules_core/sessions/views/login.php
+++ application/modules_core/sessions/views/login.php
@@ -24,7 +24,7 @@
 
 					<?php $this->load->view('system_messages'); ?>
 
-					<div class="content toggle">
+					<div class="content toggle" style="min-height: 0px;">
 
 						<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>">
 							<dl>
@@ -35,13 +35,15 @@
 								<dt><label><?php echo $this->lang->line('password'); ?>: </label></dt>
 								<dd><input type="password" value="" id="password" name="password" /></dd>
 							</dl>
-							<input type="submit" value="<?php echo $this->lang->line('log_in'); ?>" name="btn_submit" id="btn_submit" />
-
+						
                             <dl>
                                 <dt>&nbsp;</dt>
                                 <dd><?php echo anchor('sessions/recover', $this->lang->line('trouble_logging_in'), array('style'=>'font-size: 85%;')); ?></dd>
                             </dl>
 
+                            <div style="float: right; margin-right: 5px;">
+                            	<input class="uibutton" type="submit" value="<?php echo $this->lang->line('log_in'); ?>" name="btn_submit" id="btn_submit" />
+                            </div>
 						</form>
 
                         
