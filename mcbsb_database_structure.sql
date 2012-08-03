3dba7e72842cafa23794c7de4d5eeaca587e9da5
Damiano Venturin <dam@venturin.net>
2012-08-04 01:06:34 +0200

    renamed database file, removed support for hebrew and russian (they will be back soon)

--- mcbsb_database_structure.sql
+++ mcbsb_database_structure.sql
@@ -1,11 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
 -- Host: localhost
 -- Generation Time: Jul 20, 2012 at 03:49 AM
--- Server version: 5.5.24
--- PHP Version: 5.3.10-1ubuntu3.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
