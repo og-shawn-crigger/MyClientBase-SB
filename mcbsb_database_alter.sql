1b9633268f11ee849a57e5b5356574d7ef133616
Damiano Venturin <dam@venturin.net>
2012-09-07 22:55:04 +0200

    bug fixing

--- mcbsb_database_alter.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `mcb_data` WHERE `mcb_data`.`mcb_data_id` = 31;
-
-ALTER TABLE `mcb_invoices` DROP `invoice_custom_1`;
-
-ALTER TABLE  `mcb_tax_rates` CHANGE  `tax_rate_percent`  `tax_rate_percent` DECIMAL( 5, 2 ) NOT NULL;
-
-ALTER TABLE  `mcb_users` CHANGE  `client_id`  `user_client_id` INT( 11 ) NOT NULL DEFAULT  '0';
